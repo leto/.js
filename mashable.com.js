cb0a6f6090e95c9af06c10c5fed72d8b086da03f
Ryan Parman <ryan@ryanparman.com>
2011-05-19 22:09:55 -0700

    Updated the mashable.com script.

--- mashable.com.js
+++ mashable.com.js
@@ -1,11 +1,14 @@
 // Automatically expand "View As One Page" links.
-$(function() {
+(function() {
 	var node = $(document).find(".ytm-view-type"),
-	    viewType = node.html();
+	    viewType = node.html(),
+	    articleURI = window.location.href;
 
 	switch (viewType) {
 		case "View As One Page":
-			window.location.hash ="view_as_one_page-" + node.parents('.ytm-gallery-box').attr("id");
+			articleURI = window.location.href + "#view_as_one_page-" + node.parents('.ytm-gallery-box').attr("id");
 			break;
 	}
-});
+
+	window.location.href = 'https://www.readability.com/pr?url=' + encodeURIComponent(articleURI);
+})();
